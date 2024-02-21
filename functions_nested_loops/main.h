9f07f25dc2a0fc5510726fd14911c042f08c8165
Ary <alvarezaryam@gmail.com>
2024-02-21 16:52:50 -0400

    This is the main.h

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,20 +1,9 @@
 #ifndef MAIN
 #define MAIN
+#include <unistd.h>
+#include <stdio.h>
 
 int _putchar(char c);
-
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+void print_alphabet(void);
 
 #endif
