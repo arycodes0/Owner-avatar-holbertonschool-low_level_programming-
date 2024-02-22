9ca89a4924033aa5d35d913e5f7d996c4a9db5ff
Ary <alvarezaryam@gmail.com>
2024-02-21 23:29:10 -0400

    Learning about the abs function to get the absolute value of an integer

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ *_abs- Computes the absolute value of an integer
+ *n: Holds the value of an int
+ *Return: The value of n
+ */
+int _abs(int n)
+{
+	return (n = abs(n));
+}
