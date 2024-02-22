e6c6559384c2180e229687ecc7fae9a09fc32913
Ary <alvarezaryam@gmail.com>
2024-02-21 23:06:40 -0400

    Updating main.h

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,7 +1,17 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 int _putchar(char c);
 int main(void);
 void print_alphabet(void);
 void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
