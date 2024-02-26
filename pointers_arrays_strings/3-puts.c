49d78223ad07f31b14b369155fb241a14ea86ed7
Ary <alvarezaryam@gmail.com>
2024-02-26 15:47:08 -0400

    Fixing task 3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- *_puts- Prints a string
- *@str: String to print
- *Return: 0
+ * _puts - prints a string,
+ * followed by a new line, to stdout
+ * @str: string to print
+ *
+ * Return: void
  */
-
 void _puts(char *str)
 {
 	while (*str)
