125219a2f3768fc0ddd310b6e8784b66db74a437
Ary <alvarezaryam@gmail.com>
2024-03-12 21:21:37 -0400

    Updating dog.h file structure

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,13 +2,12 @@
 #define DOG_H
 
 /**
- *struct dog- This function creates a structure h
+ *struct dog - This function creates a structure h
  *named dog with it's information.
  *@name: Name of the dog
  *@age: Age of the dog
  *@owner: Name of dog owner
  */
-
 typedef struct dog
 {
 char *name;
@@ -16,12 +15,9 @@ float age;
 char *owner;
 } dog_t;
 
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
-
 #endif
