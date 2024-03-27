3f1615412be83a80fe7ed7b581a3b52162ae0892
Ary <alvarezaryam@gmail.com>
2024-03-26 20:52:49 -0400

    Function that adds a new node at the beginning of a list_t list

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -5,12 +5,13 @@
  * add_node - Function that adds a new node at the beginning of a list_t list.
  * @head: Head of the list_t list.
  * @str: The string that needs to be duplicated.
- * Return: The & of th`e new element, or NULL if failed.
+ * Return: The & of the new element, or NULL if failed.
  */
 
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
+	int length = 0;
 
 	if (*head == NULL)
 	{
@@ -23,7 +24,19 @@ list_t *add_node(list_t **head, const char *str)
 
 	new_node = malloc(sizeof(list_t)); /*allocating node*/
 
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
 	new_node->str = strdup(str); /*put in the data*/
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	new_node->len = length;
 
 	new_node->next = (*head); /*make next of new_node as head*/
 
