2b46b3b23e0bdec1a3386eb90f0e910f16347d99
Ary <alvarezaryam@gmail.com>
2024-03-28 00:02:48 -0400

    Fix segmentation fault

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -9,22 +9,28 @@
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node;
+	dlistint_t *new_node , *temp;
 
 	new_node = malloc(sizeof(dlistint_t));
 
-	while ((*head)->next != NULL)
-	{
-		(*head) = (*head)->next;
-	}
 	new_node->n = n;
-	new_node->next = NULL;
-	new_node->prev = *head;
 
-	if (*head != NULL)
+	if (*head == NULL)
+		*head = new_node;
+	else
+	{
 
-		(*head)->prev = new_node;
-	*head = new_node;
+	temp = *head;
+	while (temp->next)
+	{
+		temp = temp->next;
+	}
+
+
+
+	temp->next = new_node;
+	new_node->prev = temp;
+	}
 
 	return (new_node);
 }
