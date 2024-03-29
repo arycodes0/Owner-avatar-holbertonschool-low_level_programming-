b640ca83b2d4bed98830821bcde9f57ebe68f05f
Ary <alvarezaryam@gmail.com>
2024-03-28 20:45:56 -0400

    Add task 0 that reads a txt file and prints it

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -9,34 +9,38 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	FILE *fp;
+	char *buffer;
+	ssize_t bytes_read, bytes_written;
+
 	if (filename == NULL)
 		return (0);
 
-	FILE *fp = fopen(filename, "r");
+	fp = fopen(filename, "r");
 	if (fp == NULL)
 		return (0);
 
-	char *buffer = malloc(letters * sizeof(char));
+	buffer = malloc(letters * sizeof(char));
 	if (buffer == NULL)
 	{
 		fclose(fp);
 		return (0);
 	}
-	ssize_t bytes_read = fread(buffer, sizeof(char), letters, fp);
+	bytes_read = fread(buffer, sizeof(char), letters, fp);
 	if (bytes_read < 0)
 	{
-		fclose (fp);
-		free (buffer);
+		fclose(fp);
+		free(buffer);
 		return (0);
 	}
-	ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 	if (bytes_written != bytes_read)
 	{
-		fclose (fp);
-		free (buffer);
+		fclose(fp);
+		free(buffer);
 		return (0);
 	}
-	fclose (fp);
-	free (buffer);
+	fclose(fp);
+	free(buffer);
 	return (bytes_written);
 }
