c84e1b28545c90ebe84ffcb2ea54c4cd924f32a1
Ary <alvarezaryam@gmail.com>
2024-03-28 21:58:53 -0400

    Fixing logic on task 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -14,33 +14,43 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t bytes_read, bytes_written;
 
 	if (filename == NULL)
+	{
 		return (0);
+	}
 
 	fp = fopen(filename, "r");
 	if (fp == NULL)
-		return (0);
-
-	buffer = malloc(letters * sizeof(char));
-	if (buffer == NULL)
 	{
-		fclose(fp);
 		return (0);
 	}
-	bytes_read = fread(buffer, sizeof(char), letters, fp);
+
+	buffer = malloc(letters + 1); /*Allocate space for NULL terminator*/
+	if (buffer == NULL)
+	{
+	fclose(fp);
+		return (0);
+	}
+	bytes_read = fread(buffer, 1, letters, fp);
 	if (bytes_read < 0)
 	{
 		fclose(fp);
 		free(buffer);
 		return (0);
 	}
+
+	buffer[bytes_read] = '\0'; /*NULL terminate the buffer*/
+
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	if (bytes_written != bytes_read)
-	{
-		fclose(fp);
-		free(buffer);
-		return (0);
-	}
+
 	fclose(fp);
 	free(buffer);
-	return (bytes_written);
+
+	if (bytes_written == bytes_read)
+	{
+		return (bytes_written);
+	}
+	else
+	{
+		return (0);
+	}
 }
